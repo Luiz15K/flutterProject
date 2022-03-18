e89e2cc41100084593b587edfd328b6b0a9f4bfa
Luiz Guilherme <luizguilherme011@gmail.com>
2022-03-18 17:15:27 -0300

    Primeiro Commit

--- main.dart
+++ main.dart
@@ -1,5 +1,116 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-main() {
-  print("Hello");
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: homePage(),
+    );
+  }
+}
+
+class homePage extends StatelessWidget {
+  const homePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Finance App'),
+        backgroundColor: Colors.teal,
+        foregroundColor: Colors.amber,
+        shadowColor: Colors.amber,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add_alert),
+            tooltip: 'Alert',
+            onPressed: () {
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(const SnackBar(content: Text('Bem vindo')));
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.navigate_next),
+            tooltip: 'Opções',
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute<void>(
+                builder: (BuildContext context) {
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: const Text('Next page'),
+                      shadowColor: Colors.black,
+                      backgroundColor: Colors.teal,
+                    ),
+                    body: const Center(
+                      child: Text(
+                        'This is the next page',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    ),
+                  );
+                },
+              ));
+            },
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          'Pagina Inicial',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
+
+class AppMenu extends StatelessWidget {
+  const AppMenu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Menu do Usuário'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Início',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Mensagens'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Perfil'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Configurações'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
